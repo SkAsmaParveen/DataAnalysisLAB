94eae2077a2c559e8510ddc27a94b489e5f0fff3
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-08-24 13:14:09 +0530

    Create NumpPyOperations.py

---
+++ NumpPyOperations.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+#creating arrays
+
+arr = np.array([5,6,7,3])
+a = np.array([[1,1,2],[3,4,3]])
+
+# dimension, size and shape of the array
+
+print("The dimension is: ",arr.ndim)
+print("arr.size = The total no. of ele in the array: ",arr.size)
+print("a.shape = The no.of ele stored in each dimension",a.shape)
+
+# The main difference between the two is that range is a built-in Python class, 
+# while arange() is a function that belongs to a third-party library (NumPy). In addition, their purposes are different! Generally, 
+# range is more suitable when you need to iterate using the Python for loop.
+
+print(np.arange(1,7,2)) #arange(start, stop, step)
+print(np.zeros(6))
+print(np.ones(4))
+
+#create empty array with 3 elements
+
+print(np.empty(3))
+
+# linspace and arange : linspace allows number of steps and arange allows the size of steps
+
+print(np.arange(1,10,2)) 
+print(np.linspace(1,10,2))
+
+print("np.sort(arr): After sorting: ",np.sort(arr))
+b = np.array([[1,2],[4,5]])
+c = np.array([[1,1],[4,5]])
+print("After concatenating :\n ",np.concatenate((b,c),axis=1))  #axis=1 rowwise 0 column wise
+
+print("After reshaping : ",a.reshape(3,2))
+
+# INDEXING 
+
+d = np.array([1,2,3,4]) #one dim
+print("first ele: ",d[0])
+
+
+e = np.array([[1,2,3],[4,5,6]])
+print(e[0,1])
+
+f = np.array([[[1,2],[3,4],[5,6]],[[7,8],[9,0],[1,2]]])
+print("f[0,2,1] : ",f[0,2,1]) # 3d
+
+
+# SLICING
+
+
